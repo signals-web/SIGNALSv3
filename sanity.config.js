a9aba723638a79915ed2a211229f88b224d0e1c7
signals-web <chris.grimley@gmail.com>
2025-03-19 19:58:00 -0400

    feat: add sanity plugins for batch editing

--- sanity.config.js
+++ sanity.config.js
@@ -7,13 +7,18 @@
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
-
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './src/sanity/env'
 import schemaTypes from './src/sanity/schemaTypes'
+import {dashboardTool} from '@sanity/dashboard'
+import {documentListWidget} from 'sanity-plugin-dashboard-widget-document-list'
+import {sanityTablePlugin} from 'sanity-plugin-table'
+
+// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {structure} from './src/sanity/structure'
 
 export default defineConfig({
+  name: 'default',
+  title: 'SIGNALS',
   basePath: '/studio',
   projectId,
   dataset,
@@ -26,5 +31,15 @@ export default defineConfig({
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({defaultApiVersion: apiVersion}),
+    dashboardTool({
+      widgets: [
+        documentListWidget({
+          title: 'Last edited projects',
+          query: '*[_type == "project"] | order(_updatedAt desc)',
+          layout: { width: 'full' }
+        })
+      ]
+    }),
+    sanityTablePlugin(),
   ],
 })
