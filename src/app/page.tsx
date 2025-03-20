2daa39d327596c189c678ceb2390da2e9d4140d4
signals-web <chris.grimley@gmail.com>
2025-03-20 12:56:43 -0400

    style: update homepage with DM Sans font and centered layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import { client } from '@/sanity/lib/client'
 import { groq } from 'next-sanity'
 import Link from 'next/link'
+import { DM_Sans } from 'next/font/google'
+
+const dmSans = DM_Sans({ subsets: ['latin'] })
 
 interface Project {
   _id: string
@@ -9,14 +12,14 @@ interface Project {
 }
 
 const BookIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
-    <path fill="currentColor" d="M0 1.75A.75.75 0 0 1 .75 1h4.253c1.227 0 2.317.59 3 1.501A3.74 3.74 0 0 1 11.006 1h4.245a.75.75 0 0 1 .75.75v10.5a.75.75 0 0 1-.75.75h-4.507a2.25 2.25 0 0 0-1.591.659l-.622.621a.75.75 0 0 1-1.06 0l-.622-.621A2.25 2.25 0 0 0 5.258 13H.75a.75.75 0 0 1-.75-.75Zm7.251 10.324l.004-5.073l-.002-2.253A2.25 2.25 0 0 0 5.003 2.5H1.5v9h3.757a3.75 3.75 0 0 1 1.994.574M8.755 4.75l-.004 7.322a3.75 3.75 0 0 1 1.992-.572H14.5v-9h-3.495a2.25 2.25 0 0 0-2.25 2.25"/>
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" className="inline-block">
+    <path fill="currentColor" d="M0 1.75A.75.75 0 0 1 .75 1h4.253c1.227 0 2.317.59 3 1.501A3.743 3.743 0 0 1 11.006 1h4.245a.75.75 0 0 1 .75.75v10.5a.75.75 0 0 1-.75.75h-4.507a2.25 2.25 0 0 0-1.591.659l-.622.621a.75.75 0 0 1-1.06 0l-.622-.621A2.25 2.25 0 0 0 5.258 13H.75a.75.75 0 0 1-.75-.75Zm7.251 10.324.004-8.05A2.25 2.25 0 0 0 4.75 2H2v9h3.258a3.75 3.75 0 0 1 1.993.574ZM8.75 2.001v8.05c.149-.03.302-.046.459-.046H12V2H9.008a2.25 2.25 0 0 0-.258 0Z"/>
   </svg>
 )
 
-const FlagIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
-    <path fill="currentColor" d="M2 1.75C2 .784 2.784 0 3.75 0h8.5C13.216 0 14 .784 14 1.75v12.5A1.75 1.75 0 0 1 12.25 16h-8.5A1.75 1.75 0 0 1 2 14.25Zm1.75-.25a.25.25 0 0 0-.25.25v12.5c0 .138.112.25.25.25h8.5a.25.25 0 0 0 .25-.25V1.75a.25.25 0 0 0-.25-.25Z"/>
+const SignageIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" className="inline-block">
+    <path fill="currentColor" d="M7.75 0a.75.75 0 0 1 .75.75V3h3.634c.414 0 .814.147 1.13.414l2.07 1.75a1.75 1.75 0 0 1 0 2.672l-2.07 1.75a1.75 1.75 0 0 1-1.13.414H8.5v5.25a.75.75 0 0 1-1.5 0V10H2.75A1.75 1.75 0 0 1 1 8.25v-3.5C1 3.784 1.784 3 2.75 3H7V.75A.75.75 0 0 1 7.75 0m4.384 8.5a.25.25 0 0 0 .161-.06l2.07-1.75a.248.248 0 0 0 0-.38l-2.07-1.75a.25.25 0 0 0-.161-.06H2.75a.25.25 0 0 0-.25.25v3.5c0 .138.112.25.25.25z"/>
   </svg>
 )
 
@@ -34,27 +37,29 @@ export default async function Home() {
   const projects = await getProjects()
   
   return (
-    <main className="min-h-screen bg-[#FF0050] text-white p-8">
-      <nav className="flex justify-between items-center mb-20">
-        <h1 className="text-4xl font-bold">SIGNALS</h1>
-        <div className="space-x-4">
-          <Link href="/about" className="hover:underline">About</Link>
-          <Link href="/contact" className="hover:underline">Contact</Link>
+    <main className={`min-h-screen bg-[#FF0050] text-white ${dmSans.className}`} style={{ fontWeight: 200 }}>
+      <div className="max-w-5xl mx-auto px-4 py-8">
+        <header className="flex justify-between items-center mb-20">
+          <h1 className="text-5xl">SIGNALS</h1>
+          <nav>
+            <Link href="/about" className="mr-4">About</Link>
+            <Link href="/contact">Contact</Link>
+          </nav>
+        </header>
+        
+        <div className="text-2xl leading-relaxed">
+          {projects.map((project, index) => (
+            <span key={project._id}>
+              {index > 0 && <span className="mx-2">,</span>}
+              {project.type === 'book' ? <BookIcon /> : <SignageIcon />}
+              {' '}
+              <Link href={`/projects/${project._id}`} className="hover:underline">
+                {project.title}
+              </Link>
+            </span>
+          ))}
+          <span>.</span>
         </div>
-      </nav>
-      
-      <div className="text-2xl leading-relaxed">
-        {projects.map((project: Project, index: number) => (
-          <span key={project._id}>
-            {index > 0 && <span>, </span>}
-            {project.type === 'book' ? <BookIcon /> : <FlagIcon />}
-            {' '}
-            <Link href={`/projects/${project._id}`} className="hover:underline">
-              {project.title}
-            </Link>
-          </span>
-        ))}
-        .
       </div>
     </main>
   )
